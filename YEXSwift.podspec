f23640738c1d5595ddf34c6e01c7ebbc45ec2575
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2022-11-02 09:17:59 +0800

    font 类调用

--- YEXSwift.podspec
+++ YEXSwift.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'YEXSwift'
-  s.version          = '0.2.2'
+  s.version          = '0.2.3'
   s.summary          = 'A short description of YEXSwift.'
   s.description      = <<-DESC
                         常用扩展,基类
--- YEXSwift/Classes/Extension/UIFontExtensions.swift
+++ YEXSwift/Classes/Extension/UIFontExtensions.swift
@@ -12,29 +12,29 @@ import UIKit
 public extension YEXProtocol where T: UIFont {
 
     ///极细体
-    func thin(size: CGFloat) -> UIFont {
+    static func thin(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFang-SC-Thin", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     ///纤细体
-    func ultralight(size: CGFloat) -> UIFont {
+    static func ultralight(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFang-SC-Ultralight", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     ///中粗体
-    func semibold(size: CGFloat) -> UIFont {
+    static func semibold(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFang-SC-Semibold", size: size) ?? UIFont.boldSystemFont(ofSize: size)
     }
     ///中黑体
-    func medium(size: CGFloat) -> UIFont {
+    static func medium(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFang-SC-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     ///常规体
-    func regular(size: CGFloat) -> UIFont {
+    static func regular(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFangSC-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
     }
-    func heavy(size: CGFloat) -> UIFont {
+    static func heavy(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFang-SC-Heavy", size: size) ?? UIFont.boldSystemFont(ofSize: size)
     }
-    func bold(size: CGFloat) -> UIFont {
+    static func bold(size: CGFloat) -> UIFont {
         return UIFont(name: "PingFang-SC-Bold", size: size) ?? UIFont.boldSystemFont(ofSize: size)
     }
     
